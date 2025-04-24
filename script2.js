5c857d93671c8b516441eb7c90045ebd9a29ce38
Eva Mishevska <evakonst005@gmail.com>
2025-04-24 22:16:42 +0300

    first

---
+++ script2.js
@@ -0,0 +1,38 @@
+// ДЗ 17. массив объектов
+// Создать массив объектов. В каждом объекте должна содержаться информация 
+// о человеке: имя, пол, год рождения. Объектов должно быть 5-10.
+
+// 1.посчитать средний возраст
+// 2.определить представителей какого пола больше вывести ответы в консоль
+
+let personData = [ 
+  {userName: 'Клава', gender: 'woman', age: 98,},
+  {userName: 'Пётр', gender: 'men', age: 8,},
+  {userName: 'Валера', gender: 'men', age: 57,},
+  {userName: 'Саша', gender: 'woman', age: 29,},
+  {userName: 'Паша', gender: 'men',age: 45,},
+];
+
+let ryyAge= personData.reduce((sum, current) => sum + current.age, 0);
+let resultAge = ryyAge / personData.length;
+
+console.log(resultAge);
+
+let genderCount = {woman: 0, men: 0,};
+
+personData.forEach(personData => {
+  if (personData.gender == 'woman' ) {
+    genderCount.woman++
+  } else if (personData.gender == 'men' ) {
+    genderCount.men++
+  }
+}
+)
+
+if (genderCount.woman > genderCount.men) {
+  console.log('Женщин больше')
+} else if ( genderCount.men > genderCount.woman) {
+  console.log('Мужчин больше')
+} else {
+  console.log('Ровно')
+}
---
+++ script3.js
@@ -0,0 +1,36 @@
+// ДЗ 18. Сортировка
+
+// Написать функцию которая сортирует слова в строке по цифре которая находится в слове.
+// В рамках данной задачи цифры НЕ могут быть двухзначные и больше
+
+// function sortWords(str) {
+// 	??
+// }
+// sortWords('A3lice 2Bob Er1nst') // 'Er1nst 2Bob A3lice';
+// sortWords('2Fiona Ig7or J1amila B3ob Ali5ce') // 'J1amila 2Fiona B3ob A3lice Ig7or';
+// sortWords('Tes1t') // 'Tes1t';
+
+
+function sortWords (str) {
+    let user = str.trim().split(' ')
+    
+
+    function searchForNumbers (numder) {
+        for(let i = 0; i < numder.length; i++) {
+            let userNumber  = numder[i];
+            if (userNumber >= '0' && userNumber <= '9') {
+                return(userNumber);  
+            }
+        }
+    }
+    user.sort(function(a, b) {
+        let numderA = searchForNumbers(a);
+        let numderB = searchForNumbers(b);
+        return numderA - numderB;
+    });
+     
+    return user.join(' ');
+}
+
+alert(sortWords('2Fiona Ig7or J1amila B3ob Ali5ce '));
+
